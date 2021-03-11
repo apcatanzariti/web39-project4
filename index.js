a57291752c18d3edbd41768ab55d3c574d19b329
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-11 18:20:09 -0500

    server.listen done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('web39!');
+require('dotenv').config();
+
+const express = require('express');
+
+
+const server = express();
+
+server.use(express.json());
+
+// on Heroku machine an env variable is called "NODE_ENV" --> 'production'
+if (process.env.NODE_ENV === 'development') {
+    const cors = require('cors');
+    server.use(cors());
+}
+
+const PORT = process.env.PORT || 2000;
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
